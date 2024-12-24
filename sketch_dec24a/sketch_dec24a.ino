53ceebe4f4bb9d88291469e19c37355bf284e0c6
gulsoydan <gullsydn25@gmail.com>
2024-12-24 15:03:36 +0300

    Add files via upload

---
+++ sketch_dec24a/sketch_dec24a.ino
@@ -0,0 +1,13 @@
+<void setup() {
+  Serial.begin(9600);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+
+}
+
+void loop() {
+  String girilen_deger
+  Serial.print("DEGER = ");
+  Serial.println(6);
+
+}
